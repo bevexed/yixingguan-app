f34197f6abc1a9a6fb5cf93060eebc1d842801c8
bevexed <547551650@qq.com>
2019-02-18 18:24:33 +0800

    底部导航

--- src/components/NavFoot/NavFootPatient.jsx
+++ src/components/NavFoot/NavFootPatient.jsx
@@ -1,17 +1,53 @@
 import React, {PureComponent} from 'react';
-import {NavLink} from "react-router-dom";
+import {withRouter} from "react-router-dom";
 
 import './nav-foot.less'
 
 class NavFootPatient extends PureComponent {
+
+	navs = [
+		{
+			pathname: '/',
+			path: '首页',
+			isActive: false,
+			icon: require('./imgs/home.svg'),
+			selectedIcon: require('./imgs/home-s.svg')
+		},
+		{
+			pathname: '/doc',
+			path: '',
+			isActive: false,
+			icon: require('./imgs/doc.svg'),
+			selectedIcon: require('./imgs/doc.svg')
+		},
+		{
+			pathname: '/person',
+			path: '我的',
+			isActive: false,
+			icon: require('./imgs/my.svg'),
+			selectedIcon: require('./imgs/my-s.svg')
+		}
+	];
+
 	render() {
+		this.navs.forEach(nav => nav.isActive = this.props.location.pathname === nav.pathname);
 		return (
 			<div className='nav-foot'>
-				<NavLink to='/'>首页</NavLink>
-				<NavLink to='/person'>我的</NavLink>
+				{
+					this.navs.map(nav =>
+
+						<span
+							className={nav.isActive ? 'active' : null}
+							key={nav.pathname}
+							onClick={() => this.props.history.replace(nav.pathname)}
+						>
+							{nav.isActive ? <img src={nav.selectedIcon} alt=''/> : <img src={nav.icon} alt=''/>}{nav.path}
+						</span>
+					)
+				}
 			</div>
 		);
 	}
 }
 
-export default NavFootPatient;
+export default withRouter(NavFootPatient);
