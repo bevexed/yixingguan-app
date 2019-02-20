cc30acd60eb8e3e54f7a1fe26bcdee1c660b3736
bevexed <547551650@qq.com>
2019-02-20 15:37:18 +0800

    新建医生页面

--- src/containers/Doctors/Doctors.jsx
+++ src/containers/Doctors/Doctors.jsx
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {NavBar} from "antd-mobile";
+import {Icon, NavBar} from "antd-mobile";
 
 class Doctors extends Component {
 	render() {
 		return (
 			<div>
-				<NavBar>
+				<NavBar
+					mode={'light'}
+					rightContent={
+						<Icon
+							type={'cross'}
+							size={'md'}
+							color={'#000'}
+							style={{transform: 'rotate(45deg)'}}
+							onClick={()=>alert('add')}
+						/>}
+				>
 					我的医生
 				</NavBar>
 			</div>
