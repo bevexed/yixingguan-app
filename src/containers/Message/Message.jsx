454858d3729a222d37226f5e042cad5aa038625a
bevexed <547551650@qq.com>
2019-02-20 18:03:16 +0800

    新建 Message 页面

--- src/containers/Message/Message.jsx
+++ src/containers/Message/Message.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {NavBar,Icon} from "antd-mobile";
 
 class Message extends Component {
 	render() {
 		return (
 			<div>
-Message
+			<NavBar
+				mode="light"
+				icon={<Icon type="left" color={'#000'} size={'md'} />}
+				onLeftClick={() => this.props.history.goBack()}
+			>{this.props.match.params.to}</NavBar>
 			</div>
 		);
 	}
