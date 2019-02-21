39beafb77edb7c3771335935ced393f5d85e1c69
bevexed <547551650@qq.com>
2019-02-21 13:31:21 +0800

    注册页面

--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import './Register.less'
+import {WhiteSpace} from "antd-mobile";
 
 class Register extends Component {
 	render() {
 		return (
-			<div>
-				Register
+			<div className={'register'}>
+				<span className={'title'}>
+					为一千二百万患者提供问诊服务
+				</span>
+				<img className={'background'} src={require('../../asset/img/78-01@3x.png')} alt=""/>
+				<div
+					className={'button'}>登录/注册</div>
 			</div>
 		);
 	}
