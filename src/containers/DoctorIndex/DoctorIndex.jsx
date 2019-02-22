fd53606d82acc5165e2ac7b88c64b1e10869a736
bevexed <547551650@qq.com>
2019-02-22 11:52:04 +0800

    医生首页

--- src/containers/DoctorIndex/DoctorIndex.jsx
+++ src/containers/DoctorIndex/DoctorIndex.jsx
@@ -1,19 +1,65 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import './DoctorIndex.less'
 
-import {Result, Icon, WhiteSpace} from 'antd-mobile';
+import {
+	Result,
+	WhiteSpace,
+	List,
+	Badge,
+	SearchBar
+} from 'antd-mobile';
+
+const Item = List.Item;
+const Brief = Item.Brief;
 
 class DoctorIndex extends Component {
 	render() {
-		const myImg = src => <img src={src} className="spe am-icon am-icon-md" alt=""/>;
 		return (
-			<div className="result-example">
+			<div className="doctor-index">
 				<Result
-					img={myImg('https://gw.alipayobjects.com/zos/rmsportal/pdFARIqkrKEGVVEwotFe.svg')}
-					title="支付成功"
-					message={<div>998.00元 <del>1098元</del></div>}
+					img={<img className={'avator'} src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551407674&di=ec267c3e88e04ffe96d351b62c7a38a7&imgtype=jpg&er=1&src=http%3A%2F%2Ftx.haiqq.com%2Fuploads%2Fallimg%2F170914%2F0220055L1-9.jpg" alt=""/>}
+					title={<p className={'name'}>李医生 <img className={'sex'} src={require('./img/male@3x.png')} alt=""/></p>}
 				/>
 				<WhiteSpace/>
+
+				<List>
+					<Item
+						thumb={<img src={require('./img/Medical@3x.png')} alt=""/>}
+						arrow={'horizontal'}
+						extra={<Badge text={77} overflowCount={99}/>}
+						onClick={() => {
+						}}
+					>新增患者</Item>
+				</List>
+
+				<WhiteSpace/>
+
+				<List>
+					<Item
+					>
+						<SearchBar
+							placeholder="患者姓名"
+							maxLength={8}
+							showCancelButton
+							cancelText={<img src={require('./img/shaixuan@3x.png')} alt=""/>}
+							onCancel={() => console.log('1')}
+						/>
+						<WhiteSpace/>
+					</Item>
+					<Item
+						thumb={<img className={'patient-avator'} src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551407674&di=ec267c3e88e04ffe96d351b62c7a38a7&imgtype=jpg&er=1&src=http%3A%2F%2Ftx.haiqq.com%2Fuploads%2Fallimg%2F170914%2F0220055L1-9.jpg" alt=""/>}
+						multipleLine
+					>
+						照应听
+						<Brief>
+							<p className={'patient-message'}>
+								向您发送了一张照片 <span className={'time'}>2019-02-13 13:13</span>
+							</p>
+						</Brief>
+					</Item>
+				</List>
+
 			</div>
 		);
 	}
