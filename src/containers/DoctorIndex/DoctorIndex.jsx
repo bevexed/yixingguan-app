3571b1df513e46cfe5705462c55e95561fd38d07
bevexed <547551650@qq.com>
2019-02-22 14:45:03 +0800

    新郑 model 弹窗

--- src/containers/DoctorIndex/DoctorIndex.jsx
+++ src/containers/DoctorIndex/DoctorIndex.jsx
@@ -7,13 +7,70 @@ import {
 	WhiteSpace,
 	List,
 	Badge,
-	SearchBar
+	SearchBar,
+	Modal,
 } from 'antd-mobile';
 
 const Item = List.Item;
 const Brief = Item.Brief;
 
+const pains = [1, 2, 3, 4];
+
+function closest(el, selector) {
+	// noinspection JSUnresolvedVariable
+	const matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+	while (el) {
+		if (matchesSelector.call(el, selector)) {
+			return el;
+		}
+		el = el.parentElement;
+	}
+	return null;
+}
+
 class DoctorIndex extends Component {
+	state = {
+		modal: false,
+		pain: []
+	};
+
+	showModal = key => (e) => {
+		console.log(e);
+		// e.preventDefault(); // 修复 Android 上点击穿透
+		this.setState({
+			[key]: true,
+		});
+	};
+	onClose = key => () => {
+		this.setState({
+			[key]: false,
+		});
+	};
+
+	onWrapTouchStart = (e) => {
+		// fix touch to scroll background page on iOS
+		if (!/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
+			return;
+		}
+		const pNode = closest(e.target, '.am-modal-content');
+		if (!pNode) {
+			e.preventDefault();
+		}
+	};
+
+	selectPain = select => {
+		let pain = this.state.pain;
+		if (pain.includes(select)) {
+			let index = pain.findIndex(item => item === select);
+			pain.splice(index, 1)
+		} else {
+			pain = [...pain, select];
+			pain = Array.from(new Set(pain));
+		}
+		this.setState({pain})
+	};
+
+
 	render() {
 		return (
 			<div className="doctor-index">
@@ -43,7 +100,7 @@ class DoctorIndex extends Component {
 							maxLength={8}
 							showCancelButton
 							cancelText={<img src={require('./img/shaixuan@3x.png')} alt=""/>}
-							onCancel={() => console.log('1')}
+							onCancel={this.showModal('modal')}
 						/>
 						<WhiteSpace/>
 					</Item>
@@ -60,6 +117,40 @@ class DoctorIndex extends Component {
 					</Item>
 				</List>
 
+				<Modal
+					className={'pain'}
+					visible={this.state.modal}
+					transparent
+					maskClosable={false}
+					onClose={this.onClose('modal')}
+					title={'病症'}
+					wrapProps={{onTouchStart: this.onWrapTouchStart}}
+					afterClose={() => {
+						alert('afterClose');
+					}}
+				>
+					<div style={{height: 300, overflow: 'scroll'}}>
+						<div className={'list'}>
+							{pains.map(pain =>
+								<div className={this.state.pain.includes(pain) ? 'item active' : 'item'}
+										 key={pain}
+										 onClick={() => this.selectPain(pain)}
+								>心脏病
+								</div>)
+							}
+						</div>
+					</div>
+
+					<div className={'footer'}>
+						<div className={'button'}
+								 onClick={() => this.setState({pain: []})}>清空
+						</div>
+						<div className={'button'}
+								 onClick={this.onClose('modal')}
+						>完成
+						</div>
+					</div>
+				</Modal>
 			</div>
 		);
 	}
