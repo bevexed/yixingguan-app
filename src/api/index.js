2381f5df8acd65d2b5936b3bd5d01cca38e92a8b
bevexed <547551650@qq.com>
2019-02-25 09:51:13 +0800

    注册 / 登录接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,3 @@
-import './ajax'
+import ajax from './ajax'
+
+export const doLogin = ({phone,auto_code,identity,open_id,name}) => ajax('/api/login/do_login',{phone,auto_code,identity,open_id,name});
