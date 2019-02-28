411597b1ac17a4839e611db91ff017422b1ab475
bevexed <547551650@qq.com>
2019-02-28 15:46:42 +0800

    二次 封装 ajax loading

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 import {Toast} from "antd-mobile";
 
-export default function ajax(url, data = {}, type = "POST") {
+export default function ajax(url, data = {}, type = "POST", loading = true) {
 	return new Promise((resolve, reject) => {
 		let promise;
 		if (type === 'GET') {
@@ -19,40 +19,43 @@ export default function ajax(url, data = {}, type = "POST") {
 			error => {
 				reject(error)
 			}
-		)
+		);
+
+
+		// 添加请求拦截器
+		axios.interceptors.request.use(function (config) {
+			// 在发送请求之前做些什么
+
+			// 加载 loading 动画
+			if (loading) {
+				Toast.loading('数据加载中...', 1500, () => {
+				}, true);
+			}
+
+			return config;
+		}, function (error) {
+			// 对请求错误做些什么
+			return Promise.reject(error);
+		});
+
+		// 添加响应拦截器
+		axios.interceptors.response.use(function (response) {
+			// 对响应数据做点什么
+
+			// 关闭 Loading 状态
+			Toast.hide();
+			return response;
+		}, function (error) {
+			// 对响应错误做点什么
+
+			// 提示 数据加载失败
+			Toast.fail('数据加载失败', 1);
+
+			return Promise.reject(error);
+		});
 
 	})
 }
-
-// 添加请求拦截器
-axios.interceptors.request.use(function (config) {
-	// 在发送请求之前做些什么
-
-	// 加载 loading 动画
-	Toast.loading('数据加载中...', 1500, () => {}, true);
-
-	return config;
-}, function (error) {
-	// 对请求错误做些什么
-	return Promise.reject(error);
-});
-
-// 添加响应拦截器
-axios.interceptors.response.use(function (response) {
-	// 对响应数据做点什么
-
-	// 关闭 Loading 状态
-	Toast.hide();
-	return response;
-}, function (error) {
-	// 对响应错误做点什么
-
-	// 提示 数据加载失败
-	Toast.fail('数据加载失败', 1);
-
-	return Promise.reject(error);
-});
-
 
 // return new Promise((resolve, reject) => {
 // 	let promise;
