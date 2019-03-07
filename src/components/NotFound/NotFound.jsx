256cfc3a390c8bbf5b633b770fd83ea8db438abe
bevexed <547551650@qq.com>
2019-03-07 14:08:31 +0800

    新增  loading 页面

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,14 +1,31 @@
 import React, {PureComponent} from 'react';
 
+import {Toast} from "antd-mobile";
+
 class NotFound extends PureComponent {
-	render() {
+	state = {
+		button_show: false
+	};
+
+	componentDidMount() {
+		Toast.loading('数据加载中...', 3000, () => {
+			this.setState({button_show: true})
+		}, true);
+
 		sessionStorage.removeItem('path');
+	}
+
+	render() {
+		const {button_show} = this.state;
+		// this.props.history.replace('/');
 		return (
 			<div>
-				<div
-					className={'button'}
-					onClick={()=>this.props.history.replace('/')}>回到首页
-				</div>
+				{button_show ?
+					<div
+						className={'button'}
+						onClick={() => this.props.history.replace('/')}>回到首页
+					</div> : null
+				}
 			</div>
 		);
 	}
