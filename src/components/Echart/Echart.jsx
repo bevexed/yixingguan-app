e1fe7744e6c9e1f31817f6562491f3b5b6abf867
bevexed <547551650@qq.com>
2019-03-07 16:51:46 +0800

    删除 无用代码

--- src/components/Echart/Echart.jsx
+++ src/components/Echart/Echart.jsx
@@ -5,39 +5,7 @@ import ReactEcharts from 'echarts-for-react';
 class Echart extends Component {
 
 	state = {
-		option:  {
-			legend: {},
-			tooltip: {
-				trigger: 'axis',
-				showContent: true
-			},
-			dataset: {
-				// source: this.source
-				source:
-					[
-						['天', '02/02', '02/03', '02/04', '02/05', '02/06', '02/07'],
-						['收益金额', 41.1, 30.4, 65.1, 53.3, 83.8, 98.7],
-						['病人数量', 86.5, 92.1, 85.7, 83.1, 73.4, 55.1],
-						['邀请人数', 24.1, 67.2, 79.5, 86.4, 65.2, 82.5],
-					]
-			},
-			xAxis: {
-				type: 'category',
-				name: '时间'
-			},
-			yAxis: {
-				gridIndex: 0,
-			},
-			grid: {
-				width: '77%',
-				top: '20%'
-			},
-			series: [
-				{type: 'line', smooth: true, seriesLayoutBy: 'row'},
-				{type: 'line', smooth: true, seriesLayoutBy: 'row'},
-				{type: 'line', smooth: true, seriesLayoutBy: 'row'},
-			],
-		}
+		option: {}
 	};
 
 	componentWillMount() {
@@ -45,32 +13,36 @@ class Echart extends Component {
 			.then(
 				res => {
 					if (res.code === 1) {
-						this.setState({option: {legend: {},
-						tooltip: {
-							trigger: 'axis',
-								showContent: true
-						},
-						dataset: {
-							// source: this.source
-							source:res.data
-						},
-						xAxis: {
-							type: 'category',
-								name: '时间'
-						},
-						yAxis: {
-							gridIndex: 0,
-						},
-						grid: {
-							width: '77%',
-								top: '20%'
-						},
-						series: [
-							{type: 'line', smooth: true, seriesLayoutBy: 'row'},
-							{type: 'line', smooth: true, seriesLayoutBy: 'row'},
-							{type: 'line', smooth: true, seriesLayoutBy: 'row'},
-						],
-					}})
+						this.setState({
+							option:
+								{
+									legend: {},
+									tooltip: {
+										trigger: 'axis',
+										showContent: true
+									},
+									dataset: {
+										// source: this.source
+										source: res.data
+									},
+									xAxis: {
+										type: 'category',
+										name: '时间'
+									},
+									yAxis: {
+										gridIndex: 0,
+									},
+									grid: {
+										width: '77%',
+										top: '20%'
+									},
+									series: [
+										{type: 'line', smooth: true, seriesLayoutBy: 'row'},
+										{type: 'line', smooth: true, seriesLayoutBy: 'row'},
+										{type: 'line', smooth: true, seriesLayoutBy: 'row'},
+									],
+								}
+						})
 					}
 				}
 			)
