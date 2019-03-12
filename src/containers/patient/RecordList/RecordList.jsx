50a8336472808e06ae1bd405eecebbb13e379136
bevexed <547551650@qq.com>
2019-03-12 18:02:56 +0800

    预约受理记录

--- src/containers/patient/RecordList/RecordList.jsx
+++ src/containers/patient/RecordList/RecordList.jsx
@@ -21,7 +21,7 @@ class RecordList extends Component {
 	}
 
 	render() {
-		const {finished,unfinished} = this.state.data;
+		const {finished = [], unfinished = []} = this.state.data;
 
 		return (
 			<div className={'record-list'}>
@@ -39,58 +39,63 @@ class RecordList extends Component {
 				<WhiteSpace/>
 				<div className='recordList'>
 					<p className='recordP'>进行中</p>
-					<div className='recordDiv'>
-						<div>
-							<div>
-								<img className='appointAvatar' alt='' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3003067246,3954665585&fm=27&gp=0.jpg'/>
-								<div className='appointInfo'>
+					{
+						unfinished.map(item =>
+							<div className='recordDiv' key={item.id}>
+								<div>
 									<div>
-										<p className='appointName'>李峰</p>
-										<span className='appointSpan'>呼吸内科</span>
-										<span>主治医师</span>
+										<img className='appointAvatar' alt='' src={item.avatar}/>
+										<div className='appointInfo'>
+											<div>
+												<p className='appointName'>{item.name}</p>
+												<span className='appointSpan'>{item.department}</span>
+												<span>{item.with_title}</span>
+											</div>
+											<div>
+												<p className='appointGrade'>{item.hospital_level}</p>
+												<span>{item.affiliated_hospital}</span>
+											</div>
+										</div>
 									</div>
-									<div>
-										<p className='appointGrade'>三级甲等</p>
-										<span>浙江省立同德医院</span>
-									</div>
+									<span>{item.apply_time}</span>
+								</div>
+
+								<div className='recordBottomDiv'>
+									<p>病症描述：</p>
+									<span>{item.symptoms_described}</span>
 								</div>
 							</div>
-							<span>2019-02-13 13:13</span>
-						</div>
-
-						<div className='recordBottomDiv'>
-							<p>病症描述：</p>
-							<span>我怎么怎么，哈哈哈哈啊啊啊，可能这 里不是很好呀？</span>
-						</div>
-					</div>
+						)}
 				</div>
 				<div className='recordList'>
 					<p className='recordP' style={{background: "#FF9900"}}>历史记录</p>
-					<div className='recordDiv'>
-						<div>
-							<div>
-								<img className='appointAvatar' alt='' src='https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3003067246,3954665585&fm=27&gp=0.jpg'/>
-								<div className='appointInfo'>
+					{
+						finished.map(item =>
+							<div className='recordDiv' key={item.id}>
+								<div>
 									<div>
-										<p className='appointName'>李峰</p>
-										<span className='appointSpan'>呼吸内科</span>
-										<span>主治医师</span>
+										<img className='appointAvatar' alt='' src={item.avatar}/>
+										<div className='appointInfo'>
+											<div>
+												<p className='appointName'>{item.name}</p>
+												<span className='appointSpan'>{item.department}</span>
+												<span>{item.with_title}</span>
+											</div>
+											<div>
+												<p className='appointGrade'>{item.hospital_level}</p>
+												<span>{item.affiliated_hospital}</span>
+											</div>
+										</div>
 									</div>
-									<div>
-										<p className='appointGrade'>三级甲等</p>
-										<span>浙江省立同德医院</span>
-									</div>
+									<span>{item.apply_time}</span>
 								</div>
 
+								<div className='recordBottomDiv'>
+									<p>病症描述：</p>
+									<span>{item.symptoms_described}</span>
+								</div>
 							</div>
-							<span>2019-02-13 13:13</span>
-						</div>
-
-						<div className='recordBottomDiv'>
-							<p>病症描述：</p>
-							<span>我怎么怎么，哈哈哈哈啊啊啊，可能这 里不是很好呀？</span>
-						</div>
-					</div>
+						)}
 				</div>
 			</div>
 		);
