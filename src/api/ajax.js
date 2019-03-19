13e18424743c765fc304f7cb0fd5b22e36573a79
bevexed <547551650@qq.com>
2019-03-19 10:02:30 +0800

    删除无用 APi

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -60,26 +60,3 @@ export default function ajax(url, data = {}, type = "POST", loading = true) {
 
 	})
 }
-
-// return new Promise((resolve, reject) => {
-// 	let promise;
-// 	if (type === 'GET') {
-// 		let dataStr = '';
-// 		Object.keys(data).forEach(key => {
-// 			dataStr += key + '=' + data[key] + '&'
-// 		});
-// 		if (dataStr !== '') {
-// 			dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'));
-// 			url = url + '?' + dataStr
-// 		}
-// 		promise = axios.get(url)
-// 	} else {
-// 		promise = axios.post(url, data)
-// 	}
-// 	promise.then(response => {
-// 		resolve(response.data)
-// 	}).catch(error => {
-// 		reject(error)
-// 	})
-// })
-// }
