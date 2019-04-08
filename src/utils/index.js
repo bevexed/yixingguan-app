3a14e26537ed4c8351d5626781126b1ae93a5e5f
bevexed <547551650@qq.com>
2019-04-08 18:26:41 +0800

    state url 解密

--- src/utils/index.js
+++ src/utils/index.js
@@ -23,7 +23,8 @@ export const getRedirectTo = (type) => {
  */
 export const GetQueryString = function (name) {
 	let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-	let r = window.location.search.substr(1).match(reg);
+	let url = decodeURIComponent(window.location.search);
+	let r = url.substr(1).match(reg);
 	if (r != null) return unescape(r[2]);
 	return null;
 };
