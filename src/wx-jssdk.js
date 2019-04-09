4f1a50492a2e3e7814c667a13e32f6323a9e485f
bevexed <547551650@qq.com>
2019-04-09 18:00:10 +0800

    签名 修改

--- src/wx-jssdk.js
+++ src/wx-jssdk.js
@@ -36,9 +36,9 @@ export const wxPay = ({chat_room, money}) => {
 				wx.chooseWXPay({
 					appId: result.appid,
 					// appId: 'wx6ae88e9a0dcb59b1',
-					timestamp: result.timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-					nonceStr: result.noncestr, // 支付签名随机串，不长于 32 位
-					package: 'prepay_id=' + result.prepayid, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+					timestamp: new Data().valueOf(), // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+					nonceStr: result.nonce_str, // 支付签名随机串，不长于 32 位
+					package: 'prepay_id=' + result.prepay_id, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
 					signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
 					paySign: result.sign, // 支付签名
 					success: function (res) {
