00d5c814d9594f32671aaeda32ed0a9dfbc7a50c
bevexed <547551650@qq.com>
2019-04-09 15:34:28 +0800

    修改 认证 地址

--- src/wx-jssdk.js
+++ src/wx-jssdk.js
@@ -10,15 +10,15 @@ export const getWxConfig = () => {
 	let config = {
 		//前4个是微信验证签名必须的参数，第2-4个参数为类似上面 '/get-signature' 从node端获取的结果
 		'appId': dev.appId,
-		// 'nonceStr': 'xxx',
-		// 'signature': 'xxx',
-		// 'timestamp': 'xxx',
-		// //下面为可选参数
-		// 'debug': true, //开启 debug 模式
-		// 'jsApiList': ['chooseWXPay'], //设置所有想要使用的微信jsapi列表, 默认值为 ['onMenuShareTimeline', 'onMenuShareAppMessage']，分享到朋友圈及聊天记录
-		// 'customUrl': '' //自定义微信js链接
+		'nonceStr': 'xxx',
+		'signature': 'xxx',
+		'timestamp': 'xxx',
+		//下面为可选参数
+		'debug': true, //开启 debug 模式
+		'jsApiList': ['chooseWXPay'], //设置所有想要使用的微信jsapi列表, 默认值为 ['onMenuShareTimeline', 'onMenuShareAppMessage']，分享到朋友圈及聊天记录
+		'customUrl': '' //自定义微信js链接
 	};
-	const url = window.location.href;
+	const url = window.location.origin;
 	console.log(url);
 	reqWxConfig(url)
 		.then(
