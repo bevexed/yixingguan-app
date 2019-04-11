f7d3fc36c6769a4d9fd79057faf5e1b29ee8ea3e
bevexed <547551650@qq.com>
2019-04-11 14:22:42 +0800

    医生转诊

--- src/containers/common/SearchResult/SearchResult.jsx
+++ src/containers/common/SearchResult/SearchResult.jsx
@@ -2,11 +2,27 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import DocList from '../../../components/DocList/DocList'
 
-import {NavBar, Icon, WhiteSpace} from "antd-mobile";
+import {NavBar, Icon, WhiteSpace, Toast} from "antd-mobile";
+import {reqReferrals} from "../../../api/doctor";
+
 
 class SearchResult extends Component {
+
+	doReferrals = url => {
+		const only_no = sessionStorage.only_no_doctor;
+		reqReferrals({only_no, url})
+			.then(res => {
+				if (res.code === 1) {
+					Toast.success(res.message, 1)
+				} else {
+					Toast.fail(res.message, 1)
+				}
+			})
+	};
+
 	render() {
 		const {doctorList} = this.props;
+		const {identity} = this.props.user;
 
 		return (
 			<div>
@@ -24,7 +40,7 @@ class SearchResult extends Component {
 				<WhiteSpace/>
 				<WhiteSpace/>
 
-				<DocList doctorList={doctorList}/>
+				<DocList doctorList={doctorList} doReferrals={this.doReferrals} identity={identity}/>
 			</div>
 		);
 	}
@@ -32,6 +48,7 @@ class SearchResult extends Component {
 
 function mapStateToProps(state) {
 	return {
+		user: state.user,
 		doctorList: state.seekDoctorList
 	};
 }
