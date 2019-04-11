c968b1ba9be7f925b9ece87ab400a207ccea1936
bevexed <547551650@qq.com>
2019-04-11 09:55:31 +0800

    图片点击 放大

--- src/containers/common/Message/Message.jsx
+++ src/containers/common/Message/Message.jsx
@@ -169,7 +169,7 @@ class Message extends Component {
 										{chat.message}
 										{users.filter(user => user.username === chat.username)[0].username !== patientName && identity === 'patient' ? <div className={'reminder'}>郑重提示：线上咨询不能代替面诊，医生建议仅供参考!</div> : null}
 									</span> : null}
-									{chat.imgUrl ? <img className={...['message-img', chat.imgUrl === big_pic_url && 'big-img']} onClick={() => this.showBigPic(chat.imgUrl)} src={chat.imgUrl} alt="为了您的隐私，图片已销毁"/> : null}
+									{chat.imgUrl ? <img className={chat.imgUrl === big_pic_url ? 'big-img message-img' : 'message-img'} onClick={() => this.showBigPic(chat.imgUrl)} src={chat.imgUrl} alt="为了您的隐私，图片已销毁"/> : null}
 								</div> : null
 							}
 							<WhiteSpace/>
